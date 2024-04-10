ea34ae15a36a782fab57fc14419fc2ea56a9f943
Damiano Vitale <damianovitale@hotmail.it>
2024-04-10 16:34:16 +0200

    Aggiunta feature per interruazione programma

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,26 +8,52 @@
             contatti.Add("Damiano");
             contatti.Add("Mario");
             contatti.Add("Andrea");
-
-            Console.Write("Inserire nome Contatto: ");
-            contatti.Add(Console.ReadLine());
-
-            foreach (string contatto in contatti)
+            short scelta;
+            do
             {
-                Console.WriteLine(contatto);
-            }
-            Console.WriteLine("\nInserisci Nome da ricercare: ");
-             if (contatti.Contains(Console.ReadLine()))
-            {
-                Console.WriteLine("\nIl contatto è presente nella lista.");
-            }
-            else
-            {
-                Console.WriteLine("\nIl contatto non è presente nella lista.");
-            }
+                Console.WriteLine("1: Inserire nuovo Contatto");
+                Console.WriteLine("2: Visualizza Contatti");
+                Console.WriteLine("3: Ricerca un contatto ");
+                Console.WriteLine("4: Elimina un contato");
+                Console.WriteLine("0: ESCI DAL PROGRAMMA");
+                Console.Write("Inserisci l'opzione -> ");
+                short.TryParse(Console.ReadLine(), out scelta);
+                Console.WriteLine();
 
-            Console.WriteLine("\nInserire il nome del contatto da eliminare: ");
-            contatti.Remove(Console.ReadLine());
+                switch (scelta)
+                {
+                    case 1:
+
+                        Console.Write("Inserire nome Contatto: ");
+                        contatti.Add(Console.ReadLine());
+                        break;
+                    case 2:
+
+                        foreach (string contatto in contatti)
+                        {
+                            Console.WriteLine(contatto);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Inserisci Nome da ricercare: ");
+                        if (contatti.Contains(Console.ReadLine()))
+                        {
+                            Console.WriteLine("Il contatto è presente nella lista.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Il contatto non è presente nella lista.");
+                        }
+                        break;
+                    case 4:
+
+                        Console.WriteLine("Inserire il nome del contatto da eliminare: ");
+                        contatti.Remove(Console.ReadLine());
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (scelta != 0);
 
         }
     }
