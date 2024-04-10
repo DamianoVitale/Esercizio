8651173de67bb542a3b5353a4d0e550fa6a8faeb
Damiano Vitale <damianovitale@hotmail.it>
2024-04-10 15:30:37 +0200

    Add list of contact

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> contatti = new List<string>();
+            contatti.Add("Primo elemento");
+            contatti.Add("Secondo elemento");
+            contatti.Add("Terzo elemento");
+        }
+    }
+}
